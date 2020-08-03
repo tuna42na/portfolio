6c94030882019b74b6dd74312924ec3aaa659697
Tuna Fortuna <tuna42na@gmail.com>
2020-08-03 12:30:35 -0400

    Updates

---
+++ precache-manifest.07ac7f46773949f921d2680b4656baf3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7a9171a9e564b2cdabd0c61bebdbdd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "daa6e5ff9d01301e73ee",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "99f1b4229cdb67b91ab1",
+    "url": "/portfolio/static/css/main.99f30ac5.chunk.css"
+  },
+  {
+    "revision": "daa6e5ff9d01301e73ee",
+    "url": "/portfolio/static/js/2.668f82ee.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.668f82ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f1b4229cdb67b91ab1",
+    "url": "/portfolio/static/js/main.6e777230.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "0d6fda9133ad4c71bf59269114724472",
+    "url": "/portfolio/static/media/bass.0d6fda91.svg"
+  },
+  {
+    "revision": "e9a0db643ae7a8d87501e38b4ad4033b",
+    "url": "/portfolio/static/media/code.e9a0db64.svg"
+  },
+  {
+    "revision": "7aa280dd52d0a5f93732e170eb9a238e",
+    "url": "/portfolio/static/media/code_pen.7aa280dd.svg"
+  },
+  {
+    "revision": "370ca0416237465d31050346f50478e7",
+    "url": "/portfolio/static/media/resume.370ca041.svg"
+  },
+  {
+    "revision": "19a56cc3a3be8de0e8b19578fcf8b547",
+    "url": "/portfolio/static/media/tunalogo.19a56cc3.svg"
+  }
+]);
